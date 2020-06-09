6a3bff2ee043b91f45a19652f1def2e06ac30e6c
Ryan Gentzler <rgentzler@okpolicy.org>
2020-06-09 14:30:14 -0500

    add ojo theme and palette

---
+++ R/theme_ojo.R
@@ -0,0 +1,21 @@
+#' Style a ggplot in the OJO style
+#'
+#' Connects to the ojo database, if no connection exists, and creates a pointer to a specified table.
+#'
+#' @examples
+#' \dontrun{
+#' theme_ojo()
+#' }
+
+theme_ojo <- function() {
+  theme_bw(base_size=14, base_family="Menlo") %+replace%
+    theme(
+      panel.background  = element_blank(),
+      plot.background = element_blank(),
+      legend.background = element_rect(fill="transparent", colour=NA),
+      legend.key = element_rect(fill="transparent", colour=NA)
+    )
+}
+
+ojo_pal <- c("#F8D64E", "black", "#0D0887FF", "#6A00A8FF",
+             "#B12A90FF", "#E16462FF", "#FCA636FF", "#F0F921FF")
