71be063ae8ed8bb86ee7f8fe0e5ec0ce01cd0a79
Ryan Gentzler <rgentzler@okpolicy.org>
2020-08-10 12:07:51 -0500

    ojo_color and ojo_fill

--- R/ojo_colors.R
+++ R/ojo_colors.R
@@ -7,7 +7,7 @@
 #' ggplot(ojo_example, aes(file_year, n_cases, color = court)) +
 #'    geom_line(size = 1.5) +
 #'    theme_ojo() +
-#'    ojo_colors() +
+#'    ojo_color() +
 #'    scale_x_continuous(breaks = 2010:2019,
 #'                    limits = c(NA, 2019))
 #' }
@@ -15,8 +15,10 @@
 ojo_pal <- c("#F8D64E", "black", "#0D0887FF", "#6A00A8FF",
              "#B12A90FF", "#E16462FF", "#FCA636FF", "#F0F921FF")
 
-ojo_colors <- function(numbers = 1:8) {
-  scale_color_manual(values = ojo_pal[numbers]) +
-    scale_fill_manual(values = ojo_pal[numbers])
+ojo_color <- function(numbers = 1:8) {
+  scale_color_manual(values = ojo_pal[numbers])
 }
 
+ojo_fill <- function(numbers = 1:8) {
+  scale_fill_manual(values = ojo_pal[numbers])
+}
