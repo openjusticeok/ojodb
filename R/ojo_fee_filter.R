be0345f06b478828d2cc5208d5a0273f6476ecb2
andrewjbe <56839927+andrewjbe@users.noreply.github.com>
2021-04-05 16:15:20 -0500

    ojo_fee_filter update
    Added a few more codes to be filtered out (mostly other ways of coding incarceration / jail costs, which we want to exclude). Also added a filter eliminating observations with negative values.

--- R/ojo_fee_filter.R
+++ R/ojo_fee_filter.R
@@ -3,7 +3,7 @@
 #' Filters a data frame of fees from the OJO database using consistent criteria. ojo_fee_filter removes rows that contain certain strings and those that are over $300,000. Requires columns named 'min_desc' and 'fee_amt'.
 #'
 #' Filter code:
-#' filter(!str_detect(min_desc, "CASH BOND|FORFEIT|WARR(E|A)NT RETUR|JAIL COSTS|CREDIT TIME SERVED|PAID BY DIS|DECEASED|ADJUSTING ENTRY|CASE NOT PROCESSED"), fee_amt < 300000)
+#' filter(!str_detect(min_desc, "CASH BOND|FORFEIT|WARR(E|A)NT RETUR|JAIL COSTS|CREDIT TIME SERVED|PAID BY DIS|DECEASED|ADJUSTING ENTRY|CASE NOT PROCESSED|AC22|AC36|AC72|SFIJC|TR"), fee_amt < 300000, fee_amt > 0)
 #'
 #' @examples
 #' \dontrun{
@@ -23,13 +23,19 @@ ojo_fee_filter <- function(df) {
                     "PAID BY DIS",
                     "DECEASED",
                     "ADJUSTING ENTRY",
-                    "CASE NOT PROCESSED")
+                    "CASE NOT PROCESSED",
+                    "AC22",               # Incarceration costs
+                    "AC35",               # Incarceration costs
+                    "AC72",               # Incarceration costs (split between Sheriff and DA in Pushmataha)
+                    "SFIJC",              # Incarceration costs
+                    "TR")                 # Account transfers
 
   filter_string <- paste(filter_terms, collapse = "|")
 
   fdf <- df %>%
     filter(!str_detect(min_desc, filter_string),
-           fee_amt < 300000)
+           fee_amt < 300000,
+           fee_amt > 0)
 
   filtered_results <- df %>%
     mutate(exclusion = if_else(fee_amt > 300000,
