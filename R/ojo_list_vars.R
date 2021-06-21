22861ed31fce6ef2a86af34cbdc8048841decea1
Brancen Gregory <brancengregory@gmail.com>
2021-06-21 13:48:57 -0500

    Modified table related functions to use new db table names, changed examples

--- R/ojo_list_vars.R
+++ R/ojo_list_vars.R
@@ -1,15 +1,16 @@
 #' List the variables in an \code{ojo} database table
 #'
-#' @param table Name of the table as a string, e.g. "ojo_crim_cases"
-#' @return A character vector listing the tables in the \code{ojo} database that match the pattern
+#' @param table Name of the table as a string, e.g. "cases"
+#' @return A character vector listing the variables in the \code{ojo} database table specified
 #' @examples
 #' \dontrun{
-#' ojo_list_vars("oscn_civ_disps")
+#' ojo_list_vars("case")
 #' }
 
 ojo_list_vars <- function(table) {
+  ojodb <- ojo_connect()
 
-  d <- dbListFields(ojo_db, table)
+  d <- dbListFields(ojodb, table)
 
   return(d)
 }
