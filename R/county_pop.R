78baed0d4610f9dbefdfe4902d9ab3e8fadecb92
Brancen Gregory <brancengregory@gmail.com>
2021-06-22 09:25:37 -0500

    Fixed county_pop to use correct census data

--- R/county_pop.R
+++ R/county_pop.R
@@ -13,20 +13,21 @@ county_pop <- function(years) {
   d <- tibble()
 
   for (y in years) {
-    d <- tidycensus::get_acs("county",
-                 variables = c(pop = "B01003_001"),
-                 state = "OK",
-                 year = y) %>%
-      mutate(year = y) %>%
+    d <- tidycensus::get_estimates(product = "population",
+                                   year = y,
+                                   geography = "county",
+                                   state = "OK") |>
+      filter(variable == "POP") |>
+      mutate(year = y) |>
       bind_rows(d)
   }
 
-  d <- d %>%
-    mutate(court = NAME %>%
-             str_remove(" County.*") %>%
-             str_to_upper %>%
-             str_remove_all(" ")) %>%
-    select(court, year, pop = estimate)
+  d <- d |>
+    mutate(court = NAME |>
+             str_remove(" County.*") |>
+             str_to_upper() |>
+             str_remove_all(" ")) |>
+    select(court, year, pop = value)
 
   return(d)
 }
