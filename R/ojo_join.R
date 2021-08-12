dcd8b69631554a82b733394e51b3ebc0e99d1b1e
Brancen Gregory <brancengregory@gmail.com>
2021-08-12 16:17:46 -0500

    Created function to pull crim cases

--- R/ojo_join.R
+++ R/ojo_join.R
@@ -1,49 +1,75 @@
-# ojo_table("case") |>
-#   left_join(ojo_table("issue"), by = c("id" = "case_id")) |>
-#   select(-starts_with("created"),
-#          -starts_with("updated"),
-#          -issues) |>
-#   rename(id = id.x,
-#          issue.id = id.y)
-#
-# ojo_join <- function() {}
-#
-# library(dm)
-#
-# dm <- dbConnect(
-#   drv = RPostgres::Postgres(),
-#   host = Sys.getenv("NEW_OJO_HOST"),
-#   dbname = "ojodb",
-#   port = Sys.getenv("NEW_OJO_PORT"),
-#   user = Sys.getenv("NEW_OJO_DEFAULT_USER"),
-#   password = Sys.getenv("NEW_OJO_DEFAULT_PASS")
-# ) |>
-#   dm_from_src()
-#
-# dm <- dm |>
-#   dm_add_fk(case, id, party, case_id) |>
-#   dm_add_fk(party, id, issue, party)
-#
-# dm <- dm |>
-#   dm_add_fk(case, id, issue, case_id) |>
-#   dm_add_fk(issue, party, party, id)
-#
-# dm |>
-#   dm_draw()
-#
-# dm_flatten_to_tbl(dm, case)
-#
-# ojo_connect()
+#' Query criminal cases from ojodb
+#'
+#' Query the Open Justice Oklahoma database for criminal cases with a casetype of'CM' (misdemeanor) or 'CF' (felony)
+#'
+#' @export ojo_crim_cases ojo_add_counts
+#' @return data, a lazy tibble containing the resulting criminal cases
+#' @examples
+#' \dontrun{
+#' ojo_crim_cases()
+#' ojo_crim_cases(districts = c("TULSA", "ADAIR"))
+#' ojo_crim_cases(vars = "all")
+#' ojo_crim_cases(vars = c("updated_at", "created_at"))
+#' }
+#'
 
-ojo_crim_cases <- function() {
-  ojo_tbl("case") |>
+ojo_crim_cases <- function(districts = "all", vars = NULL, ...) {
+  data <- ojo_tbl("case") |>
     filter(case_type %in% c("CF", "CM"))
+
+  if(districts != "all") {
+    data <- data |>
+      filter(district %in% districts)
+  }
+
+  selection <- c("id", "district", "case_type", "date_filed", "date_closed")
+
+  if(is.null(vars)) {
+    data <- data |>
+      select(all_of(selection))
+    return(data)
+  } else {
+    if(vars == "all") {
+      return(data)
+    } else {
+      selection <- append(selection, vars) |>
+        unique()
+
+      data <- data |>
+        select(all_of(selection))
+
+      return(data)
+    }
+  }
 }
 
-ojo_civ_cases <- function() {
 
+ojo_add_counts <- function(data, vars = NULL, ...) {
+  if(!"tbl_lazy" %in% class(data)) {
+    stop("Don't use `collect()` before this function")
+  }
+
+  columns <- colnames(data)
+
+  counts <- ojo_tbl("count")
+
+  if(is.null(vars)) {
+    counts <- counts |>
+      select(case_id, disposition, disposition_date)
+  } else {
+    if(vars != "all") {
+      selection <- c(case_id, disposition, disposition_date, vars)
+      counts <- counts |>
+        select(all_of(selection))
+    }
+  }
+
+  data <- data |>
+    left_join(counts,
+              by = c("id" = "case_id"),
+              suffix = c("", ".count"))
+
+  return(data)
 }
 
-ojo_evictions <- function() {
 
-}
