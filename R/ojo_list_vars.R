7d3ed95d991db8d1b8f31194b7c1ca30355a4e7e
Brancen Gregory <brancengregory@gmail.com>
2021-08-16 10:22:44 -0500

    Completed function to get vars from single table

---
+++ R/ojo_list_vars.R
@@ -0,0 +1,21 @@
+#' List all variables in a table on the ojodb database
+#'
+#' Query the Open Justice Oklahoma database for the names of all variables in a table
+#'
+#' @export ojo_list_vars
+#' @return data, a tibble containing the names of all variables in a table
+#' @examples
+#' \dontrun{
+#' ojo_list_vars("case")
+#' }
+#'
+
+ojo_list_vars <- function(table) {
+  if(!exists("ojodb", where = .GlobalEnv)) {
+    ojo_connect()
+  }
+
+  ojodb |>
+    dbListFields(name = table)
+}
+
