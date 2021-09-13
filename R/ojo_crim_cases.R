7478230adaae71216d2ab8ff438b42d15d50e642
Ryan Gentzler <rgentzler@okpolicy.org>
2021-09-13 16:05:31 -0500

    add variables to ojo_add_counts

--- R/ojo_crim_cases.R
+++ R/ojo_crim_cases.R
@@ -19,10 +19,12 @@ ojo_crim_cases <- function(districts = "all", vars = NULL, case_types = c("CM", 
     filter(case_type %in% case_types,
            year %in% file_years)
 
-  if(districts != "all") {
-    data <- data |>
-      filter(district %in% districts)
-  }
+  suppressWarnings(
+    if(districts != "all") {
+      data <- data |>
+        filter(district %in% districts)
+    }
+  )
 
   selection <- c("id", "district", "case_number", "case_type", "date_filed", "date_closed")
 
@@ -57,7 +59,7 @@ ojo_add_counts <- function(data, vars = NULL, ...) {
 
   if(is.null(vars)) {
     counts <- counts |>
-      select(case_id, rank, count_as_filed, violation_of, date_of_offense,
+      select(case_id, rank, party, count_as_filed, violation_of, date_of_offense,
              count_as_disposed, disposition, disposition_detail, disposition_date)
   } else {
     if(vars != "all") {
@@ -67,10 +69,45 @@ ojo_add_counts <- function(data, vars = NULL, ...) {
     }
   }
 
+  open_counts <- ojo_tbl("case") %>%
+    filter(district == "ALFALFA",
+           case_type == "CF") %>%
+    mutate(open_count = sql("UNNEST(open_counts)"),
+           party = sql("UNNEST(parties)")) %>%
+    left_join(ojo_tbl("party"),
+              by = c("party" = "id")) %>%
+    filter(role == "Defendant") %>%
+    select(id, open_count, name, odcr_id = oscn_id)
+
   data <- data |>
     left_join(counts,
               by = c("id" = "case_id"),
-              suffix = c("", ".count"))
+              suffix = c("", ".count")) %>%
+    left_join(open_counts,
+              by = c("id" = "id")) %>%
+    left_join(ojo_tbl("party") %>%
+                select(id, name, oscn_id),
+              by = c("party" = "id")) %>%
+    group_by(id) %>%
+    mutate(count_as_filed = if_else(is.na(count_as_filed),
+                                    open_count,
+                                    count_as_filed),
+           rank = if_else(is.na(rank),
+                          row_number() - 1,
+                          rank),
+           defname = if_else(is.na(name.x),
+                             name.y,
+                             name.x),
+           oscn_id = if_else(is.na(oscn_id),
+                             odcr_id,
+                             oscn_id)
+           ) %>%
+    ungroup %>%
+    select(district, case_number, case_type, date_filed,
+           date_closed,
+           rank, defname, oscn_id, count_as_filed, violation_of,
+           date_of_offense, count_as_disposed,
+           disposition, disposition_detail, disposition_date)
 
   return(data)
 }
