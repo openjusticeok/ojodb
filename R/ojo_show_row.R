b0f7d0e0ab31b4bb50d5b402bf8f2cb45dc9b5cc
Ryan Gentzler <rgentzler@okpolicy.org>
2021-09-20 12:06:03 -0500

    copy show_row to v2.0

---
+++ R/ojo_show_row.R
@@ -0,0 +1,21 @@
+#' Browse the source court record of a row
+#'
+#' Displays the OSCN court record for a given row in a dataframe with columns named `court` and `casenum`
+#'
+#'@param df A local dataframe with columns named `court` and `casenum`
+#'@param row_number The number of the row of the case to be shown
+#'
+#' @examples
+#'
+
+ojo_show_row <- function(df, row_number) {
+  if ("court" %in% names(df) & "casenum" %in% names(df) & row_number <= nrow(df)) {
+    paste0("https://www.oscn.net/dockets/GetCaseInformation.aspx?db=",
+           df[row_number, ]$district,
+           "&number=",
+           df[row_number, ]$case_number) %>%
+      browseURL()
+  } else {
+    message("Data frame must contain columns `court` and `casenum`.")
+  }
+}
