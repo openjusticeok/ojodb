e81737f0e6ac97d69c8ddfb57ba7f8a9167b3ab7
Brancen Gregory <brancengregory@gmail.com>
2021-10-11 19:41:28 -0400

    Default to including counts

--- R/ojo_crim_cases.R
+++ R/ojo_crim_cases.R
@@ -34,7 +34,9 @@ ojo_crim_cases <- function(districts = "all", vars = NULL, case_types = c("CM", 
       ojo_add_counts()
     return(data)
   } else {
-    if(vars == "all") {
+    if(all(vars == "all")) {
+      data <- data |>
+        ojo_add_counts()
       return(data)
     } else {
       selection <- append(selection, vars) |>
@@ -60,7 +62,7 @@ ojo_add_counts <- function(data, vars = NULL, ...) {
 
   if(is.null(vars)) {
     counts <- counts |>
-      select(case_id, rank, party, count_as_filed, violation_of, date_of_offense,
+      select(case_id, number, rank, party, count_as_filed, violation_of, date_of_offense,
              count_as_disposed, disposition, disposition_detail, disposition_date)
   } else {
     if(vars != "all") {
@@ -70,41 +72,13 @@ ojo_add_counts <- function(data, vars = NULL, ...) {
     }
   }
 
-  open_counts <- ojo_tbl("case") %>%
-    mutate(open_count = sql("UNNEST(open_counts)"),
-           party = sql("UNNEST(parties)")) %>%
-    left_join(ojo_tbl("party"),
-              by = c("party" = "id")) %>%
-    filter(role == "Defendant") %>%
-    select(id, open_count, name, odcr_id = oscn_id)
-
   data <- data |>
     left_join(counts,
               by = c("id" = "case_id"),
-              suffix = c("", ".count")) %>%
-    left_join(open_counts,
-              by = c("id" = "id")) %>%
-    left_join(ojo_tbl("party") %>%
-                select(id, name, oscn_id),
-              by = c("party" = "id")) %>%
-    group_by(id) %>%
-    mutate(count_as_filed = if_else(is.na(count_as_filed),
-                                    open_count,
-                                    count_as_filed),
-           rank = if_else(is.na(rank),
-                          row_number() - 1,
-                          rank),
-           defname = if_else(is.na(name.x),
-                             name.y,
-                             name.x),
-           oscn_id = if_else(is.na(oscn_id),
-                             odcr_id,
-                             oscn_id)
-    ) %>%
-    ungroup %>%
+              suffix = c("", ".count")) |>
     select(district, case_number, case_type, date_filed,
            date_closed,
-           rank, defname, oscn_id, count_as_filed, violation_of,
+           number, rank, count_as_filed, violation_of,
            date_of_offense, count_as_disposed,
            disposition, disposition_detail, disposition_date)
 
