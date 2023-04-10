091ba9d8c6221c826e04cfdeb2745f5b0235d94b
andrewjbe <=>
2023-04-10 15:30:51 -0500

    Got really buggy progress bar working

--- R/ojo_collect.R
+++ R/ojo_collect.R
@@ -15,14 +15,9 @@
 #'
 ojo_collect <- function(query_tibble, .silent = FALSE, ...) {
 
-  # Check if pool with correct user already exists and is valid
-  if (exists("ojo_pool", envir = .ojo_env)) {
-    .con <- get("ojo_pool", envir = .ojo_env, inherits = FALSE)
-  } else {
-    .con <- ojo_connect()
-  }
+  .con <- ojo_connect()
 
-
+  # First UI chunk
   if(!.silent) {
 
     con_desc <- dbplyr::db_connection_describe(.con) |>
@@ -37,34 +32,74 @@ ojo_collect <- function(query_tibble, .silent = FALSE, ...) {
     cli::cli_rule(left = paste("Connection:", con_desc),
                   right = "{.emph ojodb {utils::packageVersion('ojodb')}}")
 
-    cli::cli_progress_step("Searching ojodb for matching results...")
-
-    t_1 <- Sys.time() # Timer start
-    n_results <- query_tibble |>
-      dplyr::tally() |>
-      dplyr::pull(var = n) |>
-      format(big.mark = ",")
-    t_2 <- Sys.time() # Timer end
-
-    # res_query <- DBI::dbSendQuery(.con, query_tibble)
-
-    cli::cli_progress_step(paste0("Found ", n_results, " matching results! Retrieving data now..."))
-
-    if(difftime(t_2, t_1, units = "secs") > 20) {
-      cli::cli_alert_warning("If the previous step took too long for your query, you can skip it by setting `.silent = TRUE`")
-    }
-
-    cli::cli_end(div_cli)
+    cli::cli_progress_step("Searching OJO database for matching results...")
 
   }
 
-  result <- query_tibble |>
-    dplyr::collect()
+  # Translate query back to SQL
+  query <- dplyr::show_query(query_tibble) |>
+    capture.output() |>
+    paste(collapse = "\n") |>
+    gsub(pattern = "^<SQL>\n", replacement = "")
+
+  # Make initial db request
+  request <- DBI::dbSendQuery(pool::localCheckout(.con), query)
+
+  # Get n rows in request results
+  t_1 <- Sys.time() # Timer start
+  n_results <- query_tibble |>
+    dplyr::tally() |>
+    dplyr::pull(var = n)
+
+  # Can't get this to work for some reason, should be faster than the above
+  # DBI::dbGetRowCount(request)
+
+  t_2 <- Sys.time() # Timer end
+
+  # Second UI chunk
+  if(!.silent){
+    cli::cli_progress_step(paste0("Found ", format(n_results, big.mark = ","), " matching results! Retrieving data now..."))
+
+    # Warning if it took more than 20 seconds
+    if(difftime(t_2, t_1, units = "secs") > 20) {
+      cli::cli_alert_warning("If the previous step took too long for your query, you can skip these progress updates by setting `.silent = TRUE`")
+    }
+  }
+
+  # Downloading from request
+  result <- NULL
+  chunk_size <- 1000
+
+  cli::cli_progress_bar("Downloading data...",
+                        type = "iterator",
+                        total = n_results)
+
+  while (!DBI::dbHasCompleted(request)) {
+    # Fetch the next chunk of data
+    chunk <- DBI::dbFetch(request, n = chunk_size)
+    if (nrow(chunk) == 0) {
+      # No more data to fetch
+      break
+    }
+
+    # Combine the data with previous chunks
+    if (is.null(result)) {
+      result <- chunk
+    } else {
+      result <- rbind(result, chunk)
+    }
+
+    # update pb
+    cli::cli_progress_update(set = nrow(result))
+  }
+
+  # Old method
+  # result <- query_tibble |>
+  #   dplyr::collect()
 
   if(!.silent) { cli::cli_progress_step("Data retrieved!") }
 
   return(result)
-
 
 }
 
