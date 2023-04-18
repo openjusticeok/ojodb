f36e2455af4285aa12301544d96a6ebaa5e07a16
andrewjbe <=>
2023-04-18 17:06:10 -0500

    Added fun loading message

--- R/zzz.R
+++ R/zzz.R
@@ -5,4 +5,18 @@
       rm("ojo_pool", envir = ojo_env())
     }
   })
+
+  # Fun message
+  ojo_art <- r"{
+                   _           _ _
+ ╔══    ══╗   ___ (_) ___   __| | |__
+ ║/      \║  / _ \| |/ _ \ / _` | '_ \
+            | (_) | | (_) | (_| | |_) |
+ ║\      /║  \___// |\___/ \__,_|_.__/
+ ╚══    ══╝     |__/
+                          version }"
+
+  ojo_art_full <- paste0(ojo_art, as.character(ojo_version()))
+  message(cat(ojo_art_full, sep = "\n"))
+
 }
