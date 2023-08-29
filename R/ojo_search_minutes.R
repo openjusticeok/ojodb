2eccbe84346735bd546b489fc0e68f7be60becac
andrewjbe <=>
2023-08-29 13:58:07 -0500

    Updated ojo_search_minutes() I added cli elements (with a .silent option to disable), and I also re-wrote the query part to allow for query strings with spaces. If the query contains a space, and doesn't contain any postgres operators like &, |, <->, or !!, the query is wrapped in single quotes so that it's parsed as one string.

--- R/ojo_search_minutes.R
+++ R/ojo_search_minutes.R
@@ -5,6 +5,7 @@
 #' @param query A term or phrase to search for using the PostgreSQL full text search syntax
 #' @param ... Placeholder
 #' @param .con The ojodb connection to use
+#' @param .silent Should the CLI be shown?
 #'
 #' @export ojo_search_minutes
 #' @return data, a lazy tibble containing the resulting minutes
@@ -28,13 +29,39 @@
 #' ojo_search_minutes("tribal <-> jurisdiction")
 #' }
 #'
-ojo_search_minutes <- function(query, ..., .con = NULL) {
+ojo_search_minutes <- function(query, ..., .con = NULL, .silent = F) {
+
   if (is.null(.con)) {
     .con <- ojo_connect()
   }
   ojo_connect()
+
+  if(!.silent){
+    # CLI
+    con_desc <- dbplyr::db_connection_describe(.con) |>
+      gsub(pattern = "postgres", replacement = "") |>
+      trimws()
+    # Styling rules
+    cli::cli_div(
+      theme = list(
+        rule = list(color = "br_yellow",
+                    "line-type" = "single"),
+        "span.grayed" = list(color = "grey")
+      )
+    )
+
+    cli::cli_rule(left = paste("Connection:", con_desc),
+                  right = "{.emph ojodb {utils::packageVersion('ojodb')}}")
+    cli::cli_alert_info("Searching OJO database for matching minutes...")
+  }
+
+  # If query contains spaces and no postgres operators, wrap it in quotes
+  query_clean <- dplyr::if_else(grepl(" ", query) & !grepl("&|\\||<->|\\!\\!", query),
+                                paste0("'", query, "'"),
+                                query)
+
   q <- glue::glue_sql(
-    "SELECT * FROM minute WHERE to_tsvector('english', description) @@ to_tsquery('english', {query});",
+    "SELECT * FROM minute WHERE to_tsvector('english', description) @@ to_tsquery('english', {query_clean});",
     .con = .con
   )
 
@@ -42,5 +69,18 @@ ojo_search_minutes <- function(query, ..., .con = NULL) {
     pool::dbGetQuery(q) |>
     dplyr::as_tibble()
 
-  return(df)
+  n_results <- nrow(df)
+
+  if(!.silent){
+    if (n_results > 0) {
+      cli::cli_alert_success(paste0("Success! ",
+                                    format(n_results, big.mark = ","),
+                                    " matching results found."))
+      return(df)
+    } else {
+      cli::cli_alert_warning("No matching results found.")
+    }
+  } else {
+    return(df)
+  }
 }
