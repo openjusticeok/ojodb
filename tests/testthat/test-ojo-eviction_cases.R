575e065f24ed780f6a709130a912728fbb629ba4
AnthonyOKC <anthonyfloresokc@gmail.com>
2024-07-31 23:07:22 -0500

    Add additional tests for ojo_eviction_cases

--- tests/testthat/test-ojo-eviction_cases.R
+++ tests/testthat/test-ojo-eviction_cases.R
@@ -5,3 +5,15 @@ test_that("ojo_eviction_cases works", {
     ojo_eviction_cases()
   })
 })
+
+test_that("ojo_eviction_cases includes additional case variables if specified", {
+  skip_on_runiverse()
+  data <- ojo_eviction_cases(more_case_variables = "judge")
+  expect_true("judge" %in% colnames(data))
+})
+
+test_that("ojo_eviction_cases includes additional issue variables if specified", {
+  skip_on_runiverse()
+  data <- ojo_eviction_cases(more_issue_variables = "disposition_date")
+  expect_true("disposition_date" %in% colnames(data))
+})
